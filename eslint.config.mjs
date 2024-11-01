a0e30da23084cd018e962019d43ea7598c60636c
Marcin Stalmach <marcin.stalmach123@outlook.com>
2024-11-01 20:12:48 +0100

    Add style for project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,27 +2,27 @@ export default [
   {
     files: ["**/*.js", "**/*.cjs", "**/*.mjs"],
     rules: {
-        "prefer-const": "warn",
-        "no-constant-binary-expression": "error",
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "off"
-        ],
-        "quotes": [
-            "error",
-            "single",
-            {"allowTemplateLiterals": true}
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "no-console": [
-            "off"
-        ]
+      "prefer-const": "warn",
+      "no-constant-binary-expression": "error",
+      "indent": [
+          "error",
+          2
+      ],
+      "linebreak-style": [
+          "off"
+      ],
+      "quotes": [
+          "error",
+          "single",
+          {"allowTemplateLiterals": true}
+      ],
+      "semi": [
+          "error",
+          "always"
+      ],
+      "no-console": [
+          "off"
+      ]
     }
   }
 ];
