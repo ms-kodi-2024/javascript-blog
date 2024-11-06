d65c74acf832218d8d8105adf340550f5b512a6e
Marcin Stalmach <marcin.stalmach123@outlook.com>
2024-11-06 14:09:53 +0100

    Add editorconfig and stylelint

--- js/script.js
+++ js/script.js
@@ -43,7 +43,7 @@ const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles';
 
-function generateTitleLinks(){
+function generateTitleLinks() {
 
   /* [DONE] remove contents of titleList */
 
